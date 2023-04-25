2a13b79b3d6242841efdc38875457453c35eb6fb
Luis Henrique Albino <luis@geovendas.com>
2023-04-25 20:02:28 -0300

    Criado consulta de cursos

--- src/main/java/com/example/application/views/course/CourseView.java
+++ src/main/java/com/example/application/views/course/CourseView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.course;
 
 import com.example.application.components.maquetech.MaqueVerticalLayout;
+import com.example.application.components.maquetech.course.CourseConsultComponent;
 import com.example.application.components.maquetech.course.CourseRegistrationComponent;
 import com.example.application.services.course.CourseService;
 import com.example.application.views.MainLayout;
@@ -18,14 +19,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CourseView extends MaqueVerticalLayout {
 
     public CourseView(@Autowired CourseService courseService) {
-
         setAlignItems(Alignment.START);
         setJustifyContentMode(JustifyContentMode.START);
 
         TabSheet tabSheet = new TabSheet();
         tabSheet.setSizeFull();
         tabSheet.add("Cadastro", new CourseRegistrationComponent(courseService));
-        tabSheet.add("Consulta", new Div(new Label("Consulta")));
+        tabSheet.add("Consulta", new CourseConsultComponent(courseService));
 
         add(tabSheet);
     }
