a35e90ce0fb735cd5c4b07416e481cb79c104807
Luis Henrique <luishenriquealbino@gmail.com>
2023-05-19 21:00:13 -0300

    Adicionado validações de adicionar e remover material

--- src/main/java/com/example/application/components/material/consult/MaterialConsultComponent.java
+++ src/main/java/com/example/application/components/material/consult/MaterialConsultComponent.java
@@ -22,8 +22,6 @@ public class MaterialConsultComponent extends VerticalLayout {
     private List<EditMaterialListener> editMaterialListenerList = new ArrayList<>();
 
     private final boolean isOnlyConsult;
-    private Button addMaterialButton;
-    private Button removeMaterialButton;
     private final UserEntity user;
     private final MaterialService materialService;
     private final MaterialFilterComponent materialFilter;
@@ -50,25 +48,11 @@ public class MaterialConsultComponent extends VerticalLayout {
         grid.addColumn(MaterialModel::getStockSafeQty).setKey("MATERIAL_STOCK_SAFE").setHeader("Estoque seguro").setTextAlign(ColumnTextAlign.CENTER).setVisible(isOnlyConsult);
         grid.addColumn(MaterialModel::getTypeDescription).setKey("MATERIAL_TYPE").setHeader("Tipo").setTextAlign(ColumnTextAlign.CENTER);
         grid.addColumn(MaterialModel::getUnitDescription).setKey("MATERIAL_UNIT").setHeader("Unidade").setTextAlign(ColumnTextAlign.CENTER);
-        grid.addComponentColumn(this::getQuantityReservationField).setKey("MATERIAL_QTY_RESERVATION").setHeader("Quantidade reserva").setTextAlign(ColumnTextAlign.CENTER).setVisible(!isOnlyConsult);
+        grid.addComponentColumn(MaterialModel::getReservationQuantityComponent).setKey("MATERIAL_QTY_RESERVATION").setHeader("Quantidade reserva").setTextAlign(ColumnTextAlign.CENTER).setVisible(!isOnlyConsult);
         grid.addComponentColumn(this::getGridActionColumn).setKey("MATERIAL_EDIT").setHeader("Ação").setTextAlign(ColumnTextAlign.CENTER);
 
         setSizeFull();
         add(btnConsult, grid);
-    }
-
-    private Component getQuantityReservationField(MaterialModel materialModel) {
-        var fieldReservationQuantity = new NumberField();
-        fieldReservationQuantity.setMin(1);
-        fieldReservationQuantity.setStep(1);
-        fieldReservationQuantity.setWidth("35%");
-        fieldReservationQuantity.setAllowedCharPattern("[0-9]");
-
-        var vlContent = new VerticalLayout();
-        vlContent.setSizeFull();
-        vlContent.setAlignItems(Alignment.CENTER);
-        vlContent.add(fieldReservationQuantity);
-        return vlContent;
     }
 
     private Component getGridActionColumn(MaterialModel materialModel) {
@@ -76,20 +60,7 @@ public class MaterialConsultComponent extends VerticalLayout {
             return new Button("Editar", event -> editMaterial(materialModel.getId()));
         }
 
-        addMaterialButton = new Button(VaadinIcon.CHECK.create());
-        addMaterialButton.addClickListener(event -> addMaterial(materialModel));
-
-        removeMaterialButton = new Button(VaadinIcon.TRASH.create());
-        removeMaterialButton.setEnabled(false);
-        removeMaterialButton.addClickListener(event -> removeMaterial(materialModel));
-
-        var hlContent = new HorizontalLayout();
-        hlContent.setSpacing(true);
-        hlContent.setAlignItems(Alignment.CENTER);
-        hlContent.setJustifyContentMode(JustifyContentMode.CENTER);
-        hlContent.add(addMaterialButton, removeMaterialButton);
-
-        return hlContent;
+        return materialModel.getActionButtons();
     }
 
     private void initFilter() {
@@ -134,17 +105,5 @@ public class MaterialConsultComponent extends VerticalLayout {
         for (var listener : this.editMaterialListenerList) {
             listener.edit(id);
         }
-    }
-
-    public void addMaterial(MaterialModel materialModel) {
-        materialModel.setOnReservation(Boolean.TRUE);
-        addMaterialButton.setEnabled(Boolean.FALSE);
-        removeMaterialButton.setEnabled(Boolean.TRUE);
-    }
-
-    public void removeMaterial(MaterialModel materialModel) {
-        materialModel.setOnReservation(Boolean.FALSE);
-        addMaterialButton.setEnabled(Boolean.TRUE);
-        removeMaterialButton.setEnabled(Boolean.FALSE);
     }
 }
--- src/main/java/com/example/application/models/material/MaterialModel.java
+++ src/main/java/com/example/application/models/material/MaterialModel.java
@@ -2,6 +2,14 @@ package com.example.application.models.material;
 
 import com.example.application.enums.material.MaterialTypeEnum;
 import com.example.application.enums.material.MaterialUnitEnum;
+import com.example.application.helpers.NotificationHelper;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,11 +29,20 @@ public class MaterialModel {
 
     private Double stockSafeQty;
 
+    private Double reservationQuantity = 0D;
+
     private MaterialTypeEnum type;
 
     private MaterialUnitEnum unit;
 
+    @Builder.Default
     private boolean onReservation = false;
+
+    private final Button addMaterialButton = new Button(VaadinIcon.CHECK.create());
+
+    private final Button removeMaterialButton = new Button(VaadinIcon.TRASH.create());
+
+    private final NumberField reservationQuantityField = new NumberField();
 
     public String getTypeDescription() {
         return this.type.getDescription();
@@ -33,5 +50,58 @@ public class MaterialModel {
 
     public String getUnitDescription() {
         return this.unit.getDescription();
+    }
+
+    public Component getReservationQuantityComponent() {
+        reservationQuantityField.setMin(1);
+        reservationQuantityField.setStep(1);
+        reservationQuantityField.setWidth("35%");
+        reservationQuantityField.setAllowedCharPattern("[0-9]");
+        reservationQuantityField.addValueChangeListener(event -> {
+            var reservationQuantity = event.getValue();
+            this.setReservationQuantity(reservationQuantity);
+        });
+
+        var vlContent = new VerticalLayout();
+        vlContent.setSizeFull();
+        vlContent.setAlignItems(FlexComponent.Alignment.CENTER);
+        vlContent.add(reservationQuantityField);
+        return vlContent;
+    }
+
+    public Component getActionButtons() {
+        removeMaterialButton.setEnabled(false);
+
+        addMaterialButton.addClickListener(event -> addMaterial());
+        removeMaterialButton.addClickListener(event -> removeMaterial());
+
+        var hlContent = new HorizontalLayout();
+        hlContent.setSpacing(true);
+        hlContent.setAlignItems(FlexComponent.Alignment.CENTER);
+        hlContent.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        hlContent.add(addMaterialButton, removeMaterialButton);
+
+        return hlContent;
+    }
+
+    public void addMaterial() {
+        var reservationQuantity = this.getReservationQuantity();
+
+        if (reservationQuantity == null || reservationQuantity < 1) {
+            NotificationHelper.error("Configure uma quantidade válida!");
+            return;
+        }
+
+        this.setOnReservation(Boolean.TRUE);
+        addMaterialButton.setEnabled(Boolean.FALSE);
+        removeMaterialButton.setEnabled(Boolean.TRUE);
+        this.reservationQuantityField.setReadOnly(Boolean.TRUE);
+    }
+
+    public void removeMaterial() {
+        this.setOnReservation(Boolean.FALSE);
+        addMaterialButton.setEnabled(Boolean.TRUE);
+        removeMaterialButton.setEnabled(Boolean.FALSE);
+        this.reservationQuantityField.setReadOnly(Boolean.FALSE);
     }
 }
