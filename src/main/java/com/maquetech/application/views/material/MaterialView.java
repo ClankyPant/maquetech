cc86f09ef995347ba28e84397de6c97f73d6608f
Luis Henrique Albino <luis@geovendas.com>
2023-05-21 17:31:27 -0300

    WIP

--- src/main/java/com/maquetech/application/views/material/MaterialView.java
+++ src/main/java/com/maquetech/application/views/material/MaterialView.java
@@ -4,7 +4,6 @@ import com.maquetech.application.components.maquetech.MaqueVerticalLayout;
 import com.maquetech.application.components.material.consult.MaterialSearchComponent;
 import com.maquetech.application.services.material.MaterialService;
 import com.maquetech.application.views.MainLayout;
-import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -18,11 +17,6 @@ public class MaterialView extends MaqueVerticalLayout {
     public MaterialView(MaterialService materialService) throws NotFoundException {
         setAlignItems(Alignment.START);
         setJustifyContentMode(JustifyContentMode.START);
-
-        TabSheet tabSheet = new TabSheet();
-        tabSheet.setSizeFull();
-        tabSheet.add("Consulta", new MaterialSearchComponent(materialService, true));
-
-        add(tabSheet);
+        add(new MaterialSearchComponent(materialService, true));
     }
 }
